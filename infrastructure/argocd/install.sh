89e4e77afd1da4ea1ef6584a125ab432e22973b2
scoobyjava <musillynn@gmail.com>
2025-09-13 08:44:35 -0700

    chore(purge): delete legacy/unused infra and docs (argocd/, keda/, docs/overhaul, k8s/shared/configmap.yaml, purge_artemis.sh, docs/CAPABILITY_GRAPH.md) as part of Artemis debranding

--- infrastructure/argocd/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Simple ArgoCD Installation Script
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.9.3/manifests/install.yaml
-kubectl apply -f argocd-install.yaml
-kubectl apply -f rbac.yaml
-kubectl apply -f applications/
-echo "ArgoCD installed! Get admin password: kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d"
